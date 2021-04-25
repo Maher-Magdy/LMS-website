a306ca94eaab3d5df5c3775b41d8b7e9fed0efb9
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-04-25 17:24:52 +0200

    test0

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express =require("express");
 const app =express();
-console.log("he")
+app.get('/'/* path or url '/' represrnts route of the website*/, /* callback function */(req, res) => {
+    // This req object has a bunch of useful propereties u can refrence documentation for more info
+    res.send('Hello World');
+});
+// Environment variable
+const port = process.env.port || 3000
+
+app.listen(port /*PortNumber*/, () => console.log(`Listeneing on port ${port}......`) /* optionally a function that called when the app starts listening to the given port */);
+
