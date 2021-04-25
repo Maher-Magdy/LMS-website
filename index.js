4ffbfb1007cd79b9da94a64fd4eca544f898677b
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-04-26 01:49:24 +0200

    server to index

--- index.js
+++ index.js
@@ -1,16 +1,142 @@
+// To validate objects
+const Joi = require('joi');             // return class
+
 const express = require('express');     // return a function
 const app = express();                  // return an object
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
+// we vave bunch of methods like corresponds to http verbs or http methods
+// app.get();
+// app.put();
+// app.post();
+// app.delete();
+
 // To respond to http get request
 app.get('/'/* path or url '/' represrnts route of the website*/, /* callback function */(req, res) => {
     // This req object has a bunch of useful propereties u can refrence documentation for more info
     res.send('Hello World');
 });
+
 // to get all courses
 app.get('/api/courses', (req, res) => {
-    res.send("kkk");
+    res.send(courses);
 });
+
+// to get single course
+// api/courses/1 to get course of id 1
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) // error 404 object not found
+    {
+        res.status(404).send('THe course with the given id was not found.');
+        return;
+    }
+    res.send(course);
+});
+
+// // we can have multiple parameters
+// // http://localhost:3000/api/posts/2021/5
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.params);  
+// });
+
+
+// Add course
+app.post('/api/courses', (req, res) => {
+    // validate request
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema);
+    //console.log(result);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    // //Manual validation 
+    // if (!req.body.name || req.body.name.length < 3)   //if name does not exist
+    // {
+    //     // 400 bad request
+    //     res.status(400).send('Name is required and should be minimum 3 characters.');
+    //     return;
+    // }
+
+
+    // create a new course object
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name // assuming that request body there's a name property
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+// Updating resources
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course 
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) // error 404 object not found
+    {
+        res.status(404).send('THe course with the given id was not found.');
+        return;
+    }
+
+    // validate 
+    // If not valid, return 400 bad request
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // Update the course 
+    // Return the updated course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+// Deleting a course
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course 
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) // error 404 object not found
+    {
+        res.status(404).send('THe course with the given id was not found.');
+        return;
+    }
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the same course
+    res.send(course);
+});
+
 // Environment variable
 const port = process.env.port || 3000
 
 app.listen(port /*PortNumber*/, () => console.log(`Listeneing on port ${port}......`) /* optionally a function that called when the app starts listening to the given port */);
 
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema);
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+const express = require('express');     // return a function
+const app = express();                  // return an object
+// To respond to http get request
+app.get('/'/* path or url '/' represrnts route of the website*/, /* callback function */(req, res) => {
+    // This req object has a bunch of useful propereties u can refrence documentation for more info
+    res.send('Hello World');
+});
+// to get all courses
+app.get('/api/courses', (req, res) => {
+    res.send("kkk");
+});
+// Environment variable
+const port = process.env.port || 3000
+
+app.listen(port /*PortNumber*/, () => console.log(`Listeneing on port ${port}......`) /* optionally a function that called when the app starts listening to the given port */);
+
