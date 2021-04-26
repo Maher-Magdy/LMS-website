0bb4bb64c17831c898c29d5f5ec897920cfd00fc
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-04-26 17:06:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
-
-
-
 const express = require('express');     // return a function
 const app = express();                  // return an object
+//main website
 // To respond to http get request
 app.get('/'/* path or url '/' represrnts route of the website*/, /* callback function */(req, res) => {
     // This req object has a bunch of useful propereties u can refrence documentation for more info
-    res.send('Hello World');
+    res.send('welcome to LMS , you can use CRUD operations using api in the website URL. ex:/api/courses/create');
 });
+
+
+
 // to get all courses
 app.get('/api/courses', (req, res) => {
-    res.send("kkk");
+    res.send("");
 });
+
+
+// to create
+app.get('/api/courses/create', (req, res) => {
+    res.send("");
+});
+
+
+// to delete
+app.get('/api/courses/delete', (req, res) => {
+    res.send("");
+});
+
+
+
+// to update
+app.get('/api/courses/update', (req, res) => {
+    res.send("");
+});
+
+
+// to get all students
+app.get('/api/students', (req, res) => {
+    res.send("");
+});
+
+// to create
+app.get('/api/students/create', (req, res) => {
+    res.send("");
+});
+
+
+
+// to delete
+app.get('/api/students/delete', (req, res) => {
+    res.send("");
+});
+
+
+
+// to update
+app.get('/api/students/update', (req, res) => {
+    res.send("");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listener
 // Environment variable
 const port = process.env.PORT || 3000;
 
