455b677f03ff3eb99fdfc229e800fd4b98655095
Maher Magdy <61353846+Maher-Magdy@users.noreply.github.com>
2021-04-27 01:41:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.post('/api/students',(req,res)=>{
     //const result=Joi.valid(req.body,schema);
     if(req)
     {
-        res.send(req);
+        console.send(req);
         //res.status(400).send("name can only have letters , apostrophe and dashes. \n code must be exactly 7 characters ");  
         return;
     }
